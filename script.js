b0c4726a94483c02f199f549cc11dac595e3de1b
LucaMarks <lucamarks416@gmail.com>
2025-09-26 11:27:05 -0400

    NoStyle firstCommit

---
+++ script.js
@@ -0,0 +1,92 @@
+
+function login(){
+    const password = document.getElementById("masterPassword").value;
+
+    //set password for now
+    const master = "TSITheBest";
+
+    if(password == master){
+        //open dashboard
+        window.location.href = "dashboard.html";
+
+    }else{document.getElementById("error").innerText = "Incorrect Password!";}
+
+}
+
+let loginButton = document.getElementById("loginButton");
+
+//wait for user to press login button
+//run login sequence when user clicks login button
+// loginButton.addEventListener("click", login());
+
+function addPassword(){
+    const title = document.getElementById("title").value;
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    if(!title || !username || !password){document.getElementById("blankField").textContent = "Fill in all fields!";}
+
+    //get Existing passwords
+    //if no existing passwords exist, define a new empty list
+    let passwords = JSON.parse(localStorage.getItem("passwords")) || [];
+    
+
+    //add given title, username & password to passwords array
+    passwords.push({title, username, password});
+
+    //save passwords to local storage
+    localStorage.setItem("passwords", JSON.stringify(passwords));
+
+    //refresh the displayed passwords & clear input fields
+    displayPasswords();
+
+    title = "";
+    username = "";
+    password = "";
+}
+
+document.getElementById("addPassword").addEventListener("click", addPassword);
+
+function displayPasswords(){
+
+    const passwordListDisplay = document.getElementById("passwordList");
+
+    //try to retrieve passwords from localStorage, otherwise take an empty list
+    let passwordList = JSON.parse(localStorage.getItem("passwords")) || [];
+
+    if(passwordList.length === 0){
+        passwordListDisplay.innerHTML = "<p>No passwords saved :(</p>"
+        return;
+    }
+
+    //atp passwords contains at least 1 set of values
+
+    let html = "";
+
+    for (let i = 0; i < passwordList.length; i++) {
+        let item = passwordList[i];
+        html += `
+        <div class="password-item">
+            <strong>${item.title}</strong><br/>
+            Username: ${item.username}<br/>
+            Password: ${item.password}
+            <br>
+            <button onclick="deletePassword(${i})">Delete</button>
+        </div>
+         `;
+}
+
+    passwordListDisplay.innerHTML = html;
+    console.log(passwordList[0]);
+}
+
+function deletePassword(index){
+
+    let passwordList = JSON.parse(localStorage.getItem("passwords")) || [];
+
+    passwordList.splice(index, 1);
+    localStorage.setItem("passwords", JSON.stringify(passwordList));
+    displayPasswords();
+}
+
+
